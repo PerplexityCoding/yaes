21d4706bcb549cc28a467f5cc16379ee5da6c34e
ymenard <ymenard.dev@gmail.com>
2020-10-07 22:55:49 +0200

    feat(options): style options

--- src/Options.vue
+++ src/Options.vue
@@ -1,7 +1,23 @@
 <template>
   <div class="options">
-    <textarea v-model="envs" />
-    <button @click="save">save</button>
+    <section>
+      <h1>
+        YAES - Configuration Page
+        <span> (* All changes will be saved directly *) </span>
+      </h1>
+
+      <div class="title">
+        Environments Configuration:
+      </div>
+      <textarea
+        class="envs"
+        v-model="envs"
+        @change="envsChanged"
+      />
+    </section>
+    <div v-if="info" class="info">
+      {{ info }}
+    </div>
   </div>
 </template>
 
@@ -12,6 +28,7 @@ export default {
   name: "OptionsPage",
   data() {
     return {
+      info: null,
       envs: null
     };
   },
@@ -33,6 +50,13 @@ export default {
     }
   },
   methods: {
+    envsChanged() {
+      this.save();
+      this.info = "Saved !";
+      setTimeout(() => {
+        this.info = null;
+      }, 3000);
+    },
     save() {
       storageSet({
         envs: this.envs
@@ -44,5 +68,42 @@ export default {
 
 <style lang="scss" scoped>
 .options {
+  max-width: 800px;
+  margin: 0 auto;
+
+  h1 {
+    margin-bottom: 10px;
+
+    span {
+      font-size: 0.8rem;
+      font-weight: normal;
+      display: block;
+    }
+  }
+
+  section {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .title {
+    font-size: 0.90rem;
+    font-weight: bold;
+    padding: 3px 0;
+  }
+
+  .envs {
+    flex: 1;
+    min-height: 300px;
+  }
+
+  .info {
+    display: inline-block;
+    border-radius: 3px;
+    background-color: #057705;
+    color: white;
+    padding: 5px 10px;
+    margin-top: 5px;
+  }
 }
 </style>
